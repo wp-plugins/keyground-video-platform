b283cad1f791992bd902ae8dfa339d0813a90bbc
keyground <keyground@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-24 14:16:16 +0000

    tagging version 0.4.3.5
    git-svn-id: https://plugins.svn.wordpress.org/keyground-video-platform/trunk@509902 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- keyground.php
+++ keyground.php
@@ -3,7 +3,7 @@
 Plugin Name: Keyground Video Platform
 Plugin URI: http://wordpress.org/extend/plugins/keyground-video-platform/
 Description: Adds Web TV functionality to your wordpress blog. This plugin integrates your wordpress to your keyground account. 
-Version: 0.4.3.4
+Version: 0.4.3.5
 Author: Keyground
 Author URI: http://www.keyground.com
 License: GPL2
@@ -29,7 +29,7 @@ class WP_Keyground
 		
 		$this->autoUpdate=get_option("kg_auto_update");
 		$this->is_admin = is_admin(); //(bool)current_user_can( 'manage_options' );
-		$this->base_url = plugins_dir_url("",__FILE__).'/';
+		$this->base_url = plugin_dir_url("",__FILE__).'/';
 		
 		if($this->is_admin){
 			$this->initAdmin();
